487f656ff17f255159bc75cfaba1e108eeec7167
Osura Liyanage <Osura Liyanage@OsuraLiyanage>
2017-07-14 15:00:04 +0530

    email

---
+++ src/main/java/com/SendMail/LegacyMail.java
@@ -0,0 +1,98 @@
+package com.SendMail;
+
+import java.util.Calendar;
+import java.util.Properties;
+import java.util.TimerTask;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class LegacyMail extends TimerTask {
+	String filename=null;
+public LegacyMail(String string) {
+		this.filename=string;
+	}
+
+@Override
+public void run() {
+	Calendar now = Calendar.getInstance();
+	System.out.println(now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE)+ ":" + now.get(Calendar.SECOND));
+	
+	
+	  Properties props = new Properties();
+      props.put("mail.smtp.host", "mail.vikas.lk");
+      props.put("mail.smtp.socketFactory.port", "25");
+      props.put("mail.smtp.socketFactory.class",
+              "javax.net.ssl.SSLSocketFactory");
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.port", "25");
+
+      Session session = Session.getDefaultInstance(props,
+          new javax.mail.Authenticator() {
+              protected PasswordAuthentication getPasswordAuthentication() {
+                  return new PasswordAuthentication("qaauto@vikas.lk","Qaauto123");
+              }
+          });
+
+      try {
+    	  
+          
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress("qaauto@vikas.lk"));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress("osura.liyanage@qnet.net"));
+
+            // Set Subject: header field
+            message.setSubject("Friday Performance Report");
+
+            // Create the message part 
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Fill the message
+            messageBodyPart.setText("Please find the attachment under attachments tab.\n"+"Please Note this is an auto generated E mail.\n"+"Thanks\n"+"Jenkins");
+          
+            
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+           
+            DataSource source = new FileDataSource("src/test/resources/Legacy/"+filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart );
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+    
+  
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+	
+}
+}
---
+++ src/main/java/com/main/Main.java
@@ -0,0 +1,41 @@
+package com.main;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+
+import com.SendMail.LegacyMail;
+import com.SendMail.NS4SMail;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+	
+		
+		  DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		    Date date1 = dateFormatter .parse("2017-07-14 14:24:30");
+
+		    //Now create the time and schedule it
+		    Timer timer1 = new Timer();
+
+		    //Use this if you want to execute it once
+		    timer1.schedule(new LegacyMail("0000.txt"), date1);
+		    timer1.schedule(new NS4SMail("0000.txt"), date1);
+		    
+		    
+		    
+		    Date date2 = dateFormatter .parse("2017-07-14 14:25:30");
+
+		    //Now create the time and schedule it
+		    Timer timer2 = new Timer();
+
+		    //Use this if you want to execute it once
+		    timer2.schedule(new LegacyMail("0100.txt"), date2);
+		    timer2.schedule(new NS4SMail("0100.txt"), date2);
+	}
+	}
+
+
